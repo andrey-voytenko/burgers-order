a2a667c42fadbf7029e6864af8484964c434c263
Andrii Voitenko <voytenko.developer@gmail.com>
2025-02-18 03:41:43 +0200

    add vercel speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import React from 'react'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Burgers order app',
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}</body>
       <Analytics/>
+      <SpeedInsights/>
     </html>
   )
 }
