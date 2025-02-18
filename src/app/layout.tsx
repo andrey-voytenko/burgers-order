4520e0c1a6a157ebe122e418c63716afb54952e3
Andrii Voitenko <voytenko.developer@gmail.com>
2025-02-18 03:30:34 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/vue';
 
 export const metadata: Metadata = {
   title: 'Burgers order app',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics/>
     </html>
   )
 }
