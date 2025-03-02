89061f4b4b819e189e843278093773cb740536fc
Andrii Voitenko <voytenko.developer@gmail.com>
2025-03-02 11:46:16 +0200

    add MESSAGE_THREAD_ID

--- src/app/api/sendMessage/route.ts
+++ src/app/api/sendMessage/route.ts
@@ -3,29 +3,38 @@ export async function POST(req: Request) {
     const { message } = await req.json();
 
     if (!message) {
-      return new Response(JSON.stringify({ error: { message: "Message is required"} }), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
+      return new Response(
+        JSON.stringify({ error: { message: 'Message is required' } }),
+        {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
     }
 
-    const response = await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        chat_id: process.env.CHAT_ID,
-        text: message,
-        parse_mode: "Markdown",
-      }),
-    });
+    const response = await fetch(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: process.env.CHAT_ID,
+          message_thread_id: process.env.MESSAGE_THREAD_ID,
+          text: message,
+          parse_mode: 'Markdown',
+        }),
+      },
+    );
 
     const result = await response.json().then((res) => res.ok);
-    return new Response(JSON.stringify(result), { status: 200, headers: { "Content-Type": "application/json" } });
-
+    return new Response(JSON.stringify(result), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
   } catch {
     return new Response(JSON.stringify('Internal Server Error'), {
       status: 500,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
   }
 }
