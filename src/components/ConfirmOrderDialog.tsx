51e4339750dd6ae2abecadeecf5c79af934a080b
Andrii Voitenko <71927293+andrey-voytenko@users.noreply.github.com>
2025-10-05 01:53:23 +0300

    Fix mobile view (#3)

--- src/components/ConfirmOrderDialog.tsx
+++ src/components/ConfirmOrderDialog.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useOrderStore } from '@/store/order';
 import ProductCounter from '@/components/ProductCounter';
 
-enum CardEnum{
+enum CardEnum {
   Regular = 'regular',
   Gold = 'gold',
   Platinum = 'platinum',
@@ -16,7 +16,7 @@ export default function ConfirmOrderDialog({
   onClose: () => void;
 }) {
   const { order, name, setName } = useOrderStore();
-  const [currentCard, setCurrentCard] = useState<CardEnum>(CardEnum.Regular)
+  const [currentCard, setCurrentCard] = useState<CardEnum>(CardEnum.Regular);
 
   useEffect(() => {
     if (order.size === 0) {
@@ -53,16 +53,32 @@ export default function ConfirmOrderDialog({
   }
 
   let sum = 0;
-  
+  const baseBtn =
+    'p-3 font-semibold text-lg rounded-xl shadow-md transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg focus:outline-none focus:ring-4';
+
   return (
     <dialog ref={dialogRef} className="p-6">
-      <div className='flex flex-row gap-4 p-3' >
-        <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-indigo-600 hover:to-purple-600 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-indigo-500/50 focus:outline-none focus:ring-4 focus:ring-indigo-400"
-                onClick={() => setCurrentCard(CardEnum.Regular)}>Без карти</button>
-        <button className="px-6 py-3 bg-gradient-to-r from-yellow-400 to-yellow-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-yellow-500 hover:to-orange-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-yellow-400/50 focus:outline-none focus:ring-4 focus:ring-yellow-400"
-                onClick={() => setCurrentCard(CardEnum.Gold)}>Золота</button>
-        <button className="px-6 py-3 bg-gradient-to-r from-gray-200 to-gray-400 text-gray-900 font-semibold text-lg rounded-xl shadow-md hover:from-gray-300 hover:to-gray-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-gray-400/50 focus:outline-none focus:ring-4 focus:ring-gray-400"
-                onClick={() => setCurrentCard(CardEnum.Platinum)}>Платинова</button>
+      <div className="flex flex-col gap-4 p-3">
+        <button
+          className={`${baseBtn} text-white bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-indigo-600 hover:to-purple-600 focus:ring-indigo-400`}
+          onClick={() => setCurrentCard(CardEnum.Regular)}
+        >
+          Без карти
+        </button>
+
+        <button
+          className={`${baseBtn} text-white bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-500 hover:to-orange-500 focus:ring-yellow-400`}
+          onClick={() => setCurrentCard(CardEnum.Gold)}
+        >
+          Золота
+        </button>
+
+        <button
+          className={`${baseBtn} text-gray-900 bg-gradient-to-r from-gray-200 to-gray-400 hover:from-gray-300 hover:to-gray-500 focus:ring-gray-400`}
+          onClick={() => setCurrentCard(CardEnum.Platinum)}
+        >
+          Платинова
+        </button>
       </div>
       <input
         className="bg-transparent border-b border-b-gray-400 focus:outline-none w-full mb-3"
@@ -76,14 +92,14 @@ export default function ConfirmOrderDialog({
         return (
           product && (
             <div
-              className="grid grid-cols-[300px_minmax(50px,100px)_50px]"
+              className="py-1 grid grid-cols-[1fr_minmax(50px,100px)_50px]"
               key={product.id}
             >
               <p className="text-left inline">
                 {product.name}{' '}
                 <span className="text-xs">{product.count ?? ''}</span>
               </p>
-              <p>{product?.prices[currentCard]}</p>
+              <p className="text-center">{product?.prices[currentCard]}</p>
               <ProductCounter product={product} />
             </div>
           )
@@ -95,13 +111,13 @@ export default function ConfirmOrderDialog({
       </div>
       <div className="flex flex-row justify-between gap-4 mt-6">
         <button
-          className="px-6 py-3 bg-gradient-to-r from-red-400 to-red-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-red-500 hover:to-red-700 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-red-500/50 focus:outline-none focus:ring-4 focus:ring-red-400"
+          className="p-3 bg-gradient-to-r from-red-400 to-red-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-red-500 hover:to-red-700 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-red-500/50 focus:outline-none focus:ring-4 focus:ring-red-400"
           onClick={() => onClose()}
         >
           Скасувати
         </button>
         <button
-          className="px-6 py-3 bg-gradient-to-r from-green-400 to-green-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-green-500 hover:to-green-700 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-green-500/50 focus:outline-none focus:ring-4 focus:ring-green-400"
+          className="p-3 bg-gradient-to-r from-green-400 to-green-600 text-white font-semibold text-lg rounded-xl shadow-md hover:from-green-500 hover:to-green-700 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-90 active:shadow-lg active:shadow-green-500/50 focus:outline-none focus:ring-4 focus:ring-green-400"
           onClick={handleConfirm}
           disabled={!name}
         >
